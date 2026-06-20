c47bdd722d01b83fd0e1a43a3ea499a27b2d03c3
Ilyes Hernandez <hdz.ilyes@gmail.com>
2026-06-20 11:39:44 +0200

    feat: scaffold the cli foundation

---
+++ src/cli.ts
@@ -0,0 +1,19 @@
+export function cli() {
+    const [input] = Deno.args;
+
+    if (!input) {
+        throw new Error("You didn't specify the markdown file")
+    }
+
+    try {
+        const fileContent = Deno.readTextFileSync(input);
+
+        console.log(fileContent);
+    } catch (err) {
+        if (err instanceof Deno.errors.NotFound) {
+            throw new Error("The file was not found in your current directory")
+        } else if (err instanceof Deno.errors.NotCapable) {
+            throw new Error("You don't have the permission required to read the file specifed")
+        }
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
+import { cli } from "./cli.ts";
+
 function main() {
-    console.log("hello world");
+    try {
+        cli()
+    } catch (err) {
+        if (err instanceof Error) {
+            console.error(err.message);
+            Deno.exit(1);
+        }
+        console.error("Something unexpected happen during the execution of the cli")
+        Deno.exit(1)
+    }
 }
 
 if (import.meta.main) main()
